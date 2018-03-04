4502133f3c2a203c4349a916dc4cd88f2b3aa277
Nikita Chernyshov <xpom55@mail.ru>
2018-03-04 16:54:51 +0300

    init data, make Polynomial_regressor and Linear_regressor for testing

---
+++ polynomial_regression.py
@@ -0,0 +1,60 @@
+# Polynomial Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values # X всегда должна быть матрица (1:2) fake
+y = dataset.iloc[:, 2].values # y всегда должен быть вектор
+
+# так как только 10 строчек информации, мы берем максимум информации
+# иначе будет не особо точная модель, а нужна точная максимально
+# плюс данные последовательные, поэтому берем всю модель!
+# Splitting the dataset into the Training set and Test set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Linear Regression to the dataset
+# создаем для сравнения работы моделей
+from sklearn.linear_model import LinearRegression
+lin_regressor = LinearRegression()
+lin_regressor.fit(X, y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_regressor = PolynomialFeatures(degree = 2) # ^2
+X_poly = poly_regressor.fit_transform(X) # почему fit и transform??? l55 7:30
+# X_poly мы создали имкуственно polynomial ^2 данные из данных X
+# 1 3 9   1 4 16   1 5 25 ...
+lin_regressor_2 = LinearRegression() # используем обычную новую линейную модель
+lin_regressor_2.fit(X_poly, y) # закидываем в линейную регрессию новые данные
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
