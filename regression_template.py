4713ba4ba70576b6d69987f55eac7f69c8e79284
Nikita Chernyshov <xpom55@mail.ru>
2018-03-04 19:48:56 +0300

    was made universal template for predicting data

--- regression_template.py
+++ regression_template.py
@@ -7,8 +7,8 @@ import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+X = dataset.iloc[:, 1:2].values # X всегда должна быть матрица (1:2) fake
+y = dataset.iloc[:, 2].values # y всегда должен быть вектор
 
 # Splitting the dataset into the Training set and Test set
 """from sklearn.cross_validation import train_test_split
@@ -23,10 +23,11 @@ sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
 # Fitting the Regression Model to the dataset
-# Create your regressor here
 
-# Predicting a new result
+
+# Predicting a new result with Polynomial Regression
 y_pred = regressor.predict(6.5)
+
 
 # Visualising the Regression results
 plt.scatter(X, y, color = 'red')
@@ -36,7 +37,8 @@ plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
 
-# Visualising the Regression results (for higher resolution and smoother curve)
+
+# Visualising the Regression results (for higher resolution and snoother curve)
 X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
@@ -45,3 +47,13 @@ plt.title('Truth or Bluff (Regression Model)')
 plt.xlabel('Position level')
 plt.ylabel('Salary')
 plt.show()
+
+
+
+
+
+
+
+
+
+
